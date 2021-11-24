516683842cdbcd8715a4c266b448d1f36ed5a851
ryo-arakawa <arkw.bump@gmail.com>
2021-11-25 04:48:27 +0900

    createAsyncThunk

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -5,6 +5,81 @@ import { PROPS_AUTHEN, PROPS_PROFILE, PROPS_NICKNAME } from "../types";
 
 const apiUrl = process.env.REACT_APP_DEV_API_URL;
 
+export const fetchAsyncLogin = createAsyncThunk(
+    "auth/post",
+    async (authen: PROPS_AUTHEN) => {
+        const res = await axios.post(`${apiUrl}authen/jwt/create`, authen, {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    return res.data;
+    }
+);
+
+export const fetchAsyncRegister = createAsyncThunk(
+    "auth/register",
+    async (auth: PROPS_AUTHEN) => {
+        const res = await axios.post(`${apiUrl}api/register/`, auth, {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    return res.data;
+    }
+);
+
+export const fetchAsyncCreateProf = createAsyncThunk(
+    "profile/post",
+    async (nickName: PROPS_NICKNAME) => {
+    const res = await axios.post(`${apiUrl}api/profile/`, nickName, {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `JWT ${localStorage.localJWT}`,
+        },
+    });
+    return res.data;
+    }
+);
+
+export const fetchAsyncUpdateProf = createAsyncThunk(
+    "profile/put",
+    async (profile: PROPS_PROFILE) => {
+        const uploadData = new FormData();
+        uploadData.append("nickName", profile.nickName);
+        profile.img && uploadData.append("img", profile.img, profile.img.name);
+        const res = await axios.put(
+        `${apiUrl}api/profile/${profile.id}/`,
+        uploadData,
+        {
+            headers: {
+            "Content-Type": "application/json",
+            Authorization: `JWT ${localStorage.localJWT}`,
+        },
+        }
+    );
+    return res.data;
+    }
+);
+
+export const fetchAsyncGetMyProf = createAsyncThunk("profile/get", async () => {
+    const res = await axios.get(`${apiUrl}api/myprofile/`, {
+    headers: {
+        Authorization: `JWT ${localStorage.localJWT}`,
+        },
+    });
+    return res.data[0];
+});
+
+export const fetchAsyncGetProfs = createAsyncThunk("profiles/get", async () => {
+    const res = await axios.get(`${apiUrl}api/profile/`, {
+    headers: {
+        Authorization: `JWT ${localStorage.localJWT}`,
+        },
+    });
+    return res.data;
+});
+
 export const authSlice = createSlice({
     name: 'auth',
     initialState: {
@@ -64,7 +139,17 @@ export const authSlice = createSlice({
     },
 });
 
-export const { fetchCredStart, fetchCredEnd, setOpenSignIn, resetOpenSignIn, setOpenSignUp, setOpenProfile, resetOpenProfile, editNickname, } = authSlice.actions;
+export const {
+    fetchCredStart,
+    fetchCredEnd,
+    setOpenSignIn,
+    resetOpenSignIn,
+    setOpenSignUp,
+    resetOpenSignUp,
+    setOpenProfile,
+    resetOpenProfile,
+    editNickname,
+} = authSlice.actions;
 
 export const selectCount = (state: RootState) => state.counter.value;
 
