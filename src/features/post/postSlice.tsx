99ec35bce73d3ac13480d9cf8faedff0de9996f2
ryo-arakawa <arkw.bump@gmail.com>
2021-11-30 03:13:33 +0900

    feat: A new feature postSlice.tsxの設定1

---
+++ src/features/post/postSlice.tsx
@@ -0,0 +1,16 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
+import axios from "axios";
+import { PROPS_NEWPOST, PROPS_LIKED, PROPS_COMMENT } from "../types";
+
+const apiUrlPost = `${process.env.REACT_APP_DEV_API_URL}api/post/`;
+
+export const fetchAsyncGetPosts = createAsyncThunk("post/get", async () => {
+  const res = await axios.get(apiUrlPost, {
+    headers: {
+      Authorization: `JWT ${localStorage.localJWT}`,
+    },
+  });
+  return res.data;
+});
+
