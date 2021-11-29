45e54f23e4e61a0841760eb3557ea12b32478c8e
ryo-arakawa <arkw.bump@gmail.com>
2021-11-30 03:50:32 +0900

    feat: A new feature
    Coreコンポーネントの設定2

--- src/features/core/Core.tsx
+++ src/features/core/Core.tsx
@@ -5,6 +5,41 @@ import styles from "./Core.module.css";
 import { useSelector, useDispatch } from "react-redux";
 import { AppDispatch } from "../../app/store";
 
+import { withStyles } from "@material-ui/core/styles";
+import {
+  Button,
+  Grid,
+  Avatar,
+  Badge,
+  CircularProgress,
+} from "@material-ui/core";
+
+import { MdAddAPhoto } from "react-icons/md";
+
+import {
+  editNickname,
+  selectProfile,
+  selectIsLoadingAuth,
+  setOpenSignIn,
+  resetOpenSignIn,
+  setOpenSignUp,
+  resetOpenSignUp,
+  setOpenProfile,
+  resetOpenProfile,
+  fetchAsyncGetMyProf,
+  fetchAsyncGetProfs,
+} from "../auth/authSlice";
+
+import {
+  selectPosts,
+  selectIsLoadingPost,
+  setOpenNewPost,
+  resetOpenNewPost,
+  fetchAsyncGetPosts,
+  fetchAsyncGetComments,
+} from "../post/postSlice";
+
+
 const Core: React.FC = () => {
     return (
         <div>
