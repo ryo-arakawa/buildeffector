a48f80cc8f60e97be246d4e24f56dd1815927e10
ryo-arakawa <arkw.bump@gmail.com>
2021-11-30 03:42:51 +0900

    feat: A new feature
    postSlice.tsxの設定5

--- src/features/post/postSlice.tsx
+++ src/features/post/postSlice.tsx
@@ -91,3 +91,92 @@ export const fetchAsyncPostComment = createAsyncThunk(
     return res.data;
   }
 );
+
+export const postSlice = createSlice({
+  name: "post",
+  initialState: {
+    isLoadingPost: false,
+    openNewPost: false,
+    posts: [
+      {
+        id: 0,
+        title: "",
+        userPost: 0,
+        created_on: "",
+        img: "",
+        liked: [0],
+      },
+    ],
+    comments: [
+      {
+        id: 0,
+        text: "",
+        userComment: 0,
+        post: 0,
+      },
+    ],
+  },
+  reducers: {
+    fetchPostStart(state) {
+      state.isLoadingPost = true;
+    },
+    fetchPostEnd(state) {
+      state.isLoadingPost = false;
+    },
+    setOpenNewPost(state) {
+      state.openNewPost = true;
+    },
+    resetOpenNewPost(state) {
+      state.openNewPost = false;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchAsyncGetPosts.fulfilled, (state, action) => {
+      return {
+        ...state,
+        posts: action.payload,
+      };
+    });
+    builder.addCase(fetchAsyncNewPost.fulfilled, (state, action) => {
+      return {
+        ...state,
+        posts: [...state.posts, action.payload],
+      };
+    });
+    builder.addCase(fetchAsyncGetComments.fulfilled, (state, action) => {
+      return {
+        ...state,
+        comments: action.payload,
+      };
+    });
+    builder.addCase(fetchAsyncPostComment.fulfilled, (state, action) => {
+      return {
+        ...state,
+        comments: [...state.comments, action.payload],
+      };
+    });
+    builder.addCase(fetchAsyncPatchLiked.fulfilled, (state, action) => {
+      return {
+        ...state,
+        posts: state.posts.map((post) =>
+          post.id === action.payload.id ? action.payload : post
+        ),
+      };
+    });
+  },
+});
+
+export const {
+  fetchPostStart,
+  fetchPostEnd,
+  setOpenNewPost,
+  resetOpenNewPost,
+} = postSlice.actions;
+
+export const selectIsLoadingPost = (state: RootState) =>
+  state.post.isLoadingPost;
+export const selectOpenNewPost = (state: RootState) => state.post.openNewPost;
+export const selectPosts = (state: RootState) => state.post.posts;
+export const selectComments = (state: RootState) => state.post.comments;
+
+export default postSlice.reducer;
