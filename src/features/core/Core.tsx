3ce17ac320cf50f140fb35fc7365d13443832971
ryo-arakawa <arkw.bump@gmail.com>
2021-11-30 03:48:18 +0900

    feat: A new feature
    Coreコンポーネントの設定1

--- src/features/core/Core.tsx
+++ src/features/core/Core.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Auth from "../auth/Auth";
 
+import styles from "./Core.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { AppDispatch } from "../../app/store";
+
 const Core: React.FC = () => {
     return (
         <div>
