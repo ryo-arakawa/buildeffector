2e88ecc72b74570ccf657695b17b1ac3b0a8bc12
ryo-arakawa <arkw.bump@gmail.com>
2021-11-29 20:39:30 +0900

    feat: A new feature
    Coreをimport

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import styles from "./App.module.css"
+import Core from "./features/core/Core";
 
 function App() {
-  return <div className={styles.app}></div>;
+  return <div className={styles.app}>
+    <Core />
+
+  </div>;
 }
 
 export default App;
