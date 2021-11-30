a3e65db40882ef26a9b26188553edf96fcd75ff7
ryo-arakawa <arkw.bump@gmail.com>
2021-11-30 15:51:56 +0900

    feat: A new feature
    Post/EditProfile/NewPostのimport

--- src/features/core/Core.tsx
+++ src/features/core/Core.tsx
@@ -39,9 +39,9 @@ import {
   fetchAsyncGetComments,
 } from "../post/postSlice";
 
-// import Post from "../post/Post";
-// import EditProfile from "./EditProfile";
-// import NewPost from "./NewPost";
+import Post from "../post/Post";
+import EditProfile from "./EditProfile";
+import NewPost from "./NewPost";
 
 const StyledBadge = withStyles((theme) => ({
   badge: {
@@ -99,8 +99,8 @@ const Core: React.FC = () => {
   return (
     <div>
       <Auth />
-      {/* <EditProfile /> */}
-      {/* <NewPost /> */}
+      <EditProfile />
+      <NewPost />
       <div className={styles.core_header}>
         <h1 className={styles.core_title}>Build effector</h1>
         {profile?.nickName ? (
@@ -178,14 +178,14 @@ const Core: React.FC = () => {
                 .reverse()
                 .map((post) => (
                   <Grid key={post.id} item xs={12} md={4}>
-                    {/* <Post
+                    <Post
                       postId={post.id}
                       title={post.title}
                       loginId={profile.userProfile}
                       userPost={post.userPost}
                       imageUrl={post.img}
                       liked={post.liked}
-                    /> */}
+                    />
                   </Grid>
                 ))}
             </Grid>
